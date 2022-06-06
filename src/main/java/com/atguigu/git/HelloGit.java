67c25fb8954403b6285a00640e8a4b333f44bb21
china-521 <3052236335@qq.com>
2022-06-06 17:46:45 +0800

    commit master-1

---
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -0,0 +1,16 @@
+package com.atguigu.git;
+
+/**
+ * @ClassName HelloGit
+ * @Description TODO
+ * @Author wk
+ * @Date 2022/6/6 12:50
+ * @Version 1.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+    }
+}
