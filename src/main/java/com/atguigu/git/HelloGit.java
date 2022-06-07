16af590aab15ff7484ce2d6694909bcbe3eb0bc3
☆往事随風☆ <72770576+china-521@users.noreply.github.com>
2022-06-07 16:08:26 +0800

    update hellogit.java

--- src/main/java/com/atguigu/git/HelloGit.java
+++ src/main/java/com/atguigu/git/HelloGit.java
@@ -16,5 +16,6 @@ public class HelloGit {
         System.out.println("master-test");
         System.out.println("hot-fix-test");
         System.out.println("ssh-git-test");
+        System.out.println("pull-git-test");
     }
 }
